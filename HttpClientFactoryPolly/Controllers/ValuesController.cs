17cb3ffc1f4e68f7242765ec77ec4a8ddf19e306
辉 冯 <695979933@qq.com>
2019-09-19 12:59:45 +0800

    添加项目文件。

---
+++ HttpClientFactoryPolly/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpClientFactoryPolly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private static int myCount = 0;
+        public ValuesController(IHttpClientFactory httpClientFactory)
+        {
+            this._httpClientFactory = httpClientFactory;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var result = await client.GetStringAsync("https://www.microsoft.com/zh-cn/");
+            return result;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+       "repos/aspnet/docs/pulls");
+
+            var client = _httpClientFactory.CreateClient("github");
+
+            var response = await client.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+
+        [HttpGet("testtimeout")]
+        public ActionResult<IEnumerable<string>> Timeout()
+        {
+            if (myCount < 3)//模拟超时
+            {
+                System.Threading.Thread.Sleep(3000);
+            }
+            myCount++;
+
+            return new string[] { "value1", "value2" };
+        }
+        [HttpGet("timeout")]
+        public async Task<ActionResult<string>> Gets() {
+            var client = _httpClientFactory.CreateClient("test");
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/values/testtimeout");
+            var response = await client.SendAsync(request);
+            var content = await response.Content.ReadAsStringAsync();
+
+            return content;
+        }
+
+    }
+}
---
+++ HttpClientFactoryPolly/Startup.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using System;
+using System.Net.Http;
+
+namespace HttpClientFactoryPolly
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var fallbackResponse = new HttpResponseMessage();
+            fallbackResponse.Content = new StringContent("fallback");
+            fallbackResponse.StatusCode = System.Net.HttpStatusCode.TooManyRequests;
+            // services.AddHttpClient();
+            //重试
+            //services.AddHttpClient("github", c =>
+            //{
+            //    //基址
+            //    c.BaseAddress = new System.Uri("https://api.github.com/");
+            //    // Github API versioning
+            //    c.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+            //    // Github requires a user-agent
+            //    c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+            //    //AddTransientHttpErrorPolicy主要是处理Http请求的错误，如HTTP 5XX 的状态码，HTTP 408 的状态码 以及System.Net.Http.HttpRequestException异常
+            //}).AddTransientHttpErrorPolicy(p =>
+            ////WaitAndRetryAsync参数的意思是：每次重试时等待的睡眠持续时间。
+            //p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+            services.AddHttpClient("test", c =>
+            {
+                //基址
+                c.BaseAddress = new System.Uri("http://localhost:5000/");
+                // Github API versioning
+                c.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+                // Github requires a user-agent
+                c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
+            })
+          //降级
+          .AddPolicyHandler(Policy<HttpResponseMessage>.Handle<Exception>().FallbackAsync(fallbackResponse, async b =>
+             {
+                 Console.WriteLine($"fallback here {b.Exception.Message}");
+             }))
+            //熔断
+            .AddPolicyHandler(Policy<HttpResponseMessage>.Handle<Exception>().CircuitBreakerAsync(2, TimeSpan.FromSeconds(4), (ex, ts) =>
+            {
+                Console.WriteLine($"break here {ts.TotalMilliseconds}");
+            }, () =>
+            {
+                Console.WriteLine($"reset here ");
+            }))
+            //超时
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+
+                }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
